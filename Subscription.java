e2e964d760bf51a035d92aa9e616ea9de62853a5
harika nandra <128826606+harikanandra@users.noreply.github.com>
2024-01-30 21:16:12 +0530

    import java.util.*; public class Design{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int r=sc.nextInt();         if(r<40)         System.out.println("NO");         else         System.out.println("YES");     } }

---
+++ Subscription.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Design{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        if(r<40)
+        System.out.println("NO");
+        else
+        System.out.println("YES");
+    }
+}
