71ca5a3b330b5783aaa8b497a1dd27b40203b3e5
harika nandra <128826606+harikanandra@users.noreply.github.com>
2024-01-30 21:10:01 +0530

    import java.util.*; public class Design{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x1=sc.nextInt();         if(x1>98)         System.out.println("YES");         else         System.out.println("NO");     } }

---
+++ Fever.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Design{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x1=sc.nextInt();
+        if(x1>98)
+        System.out.println("YES");
+        else
+        System.out.println("NO");
+    }
+}
