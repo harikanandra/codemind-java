1ace08a6712ba03678623f85a346b89df416c294
harika nandra <128826606+harikanandra@users.noreply.github.com>
2024-01-30 21:12:44 +0530

    import java.util.*; public class Design{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int r=sc.nextInt();         if(r<3)         System.out.println("LIGHT");         else if(r>=3 && r<7)         System.out.println("MODERATE");         else         System.out.println("HEAVY");     } }

---
+++ Rain_Fall.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Design{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        if(r<3)
+        System.out.println("LIGHT");
+        else if(r>=3 && r<7)
+        System.out.println("MODERATE");
+        else
+        System.out.println("HEAVY");
+    }
+}
