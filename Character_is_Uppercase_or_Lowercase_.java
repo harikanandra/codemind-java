ae9ab64ea1bb68a059690ae15aa9a4d94ecaa310
harika nandra <128826606+harikanandra@users.noreply.github.com>
2024-02-01 10:10:12 +0530

    import java.util.*;
    public class Marks {
        public static void main(String args[]) {
            Scanner sc = new Scanner(System.in);
            char a = sc.next().charAt(0);
            if(a>='A' && a<='Z')
            System.out.println("uppercase alphabet");
            else if(a>='a' && a<='z')
            System.out.println("lowercase alphabet");
            else
            System.out.println("not an alphabet");
        }
    }

---
+++ Character_is_Uppercase_or_Lowercase_.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Marks {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next().charAt(0);
+        if(a>='A' && a<='Z')
+        System.out.println("uppercase alphabet");
+        else if(a>='a' && a<='z')
+        System.out.println("lowercase alphabet");
+        else
+        System.out.println("not an alphabet");
+    }
+}
