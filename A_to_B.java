ac51e5c914e1424fa86211a716bcd74037eddb80
harika nandra <128826606+harikanandra@users.noreply.github.com>
2024-02-01 20:28:32 +0530

    import java.util.*; public class Sum{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int a=sc.nextInt();         for(int i=n;i<=a;i++)         {         System.out.printf("%d ",i);         }     } }

---
+++ A_to_B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Sum{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=sc.nextInt();
+        for(int i=n;i<=a;i++)
+        {
+        System.out.printf("%d ",i);
+        }
+    }
+}
