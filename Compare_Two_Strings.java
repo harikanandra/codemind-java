a9d850014b18cca31359378c74a72016675465a2
harika nandra <128826606+harikanandra@users.noreply.github.com>
2024-02-16 12:02:32 +0530

    import java.util.*; public class Strings{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.next();         String s1=sc.next();         if(s.equals(s1))         System.out.println("Strings are Equal");         else         System.out.println("Strings are not Equal");     } }

---
+++ Compare_Two_Strings.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Strings{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        String s1=sc.next();
+        if(s.equals(s1))
+        System.out.println("Strings are Equal");
+        else
+        System.out.println("Strings are not Equal");
+    }
+}
